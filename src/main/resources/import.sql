ab5eb897fe3a304b3d690feb84de01e25a453816
Carlos Estay Olguín <cfestay@gmail.com>
2021-08-02 11:37:03 -0400

    cambio de bbdd a postgresql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,10 +1,10 @@
 /*Contactos vip*/
 
-insert into contacts (first_name, last_name, init_date) values ("Clint", "Eastwood", "1930-05-30");
-insert into contacts (first_name, last_name, init_date) values ("Arnold", "Schwarzenegger", "1947-07-27");
-insert into contacts (first_name, last_name, init_date) values ("Jennifer", "Connelly", "1970-12-11");
-insert into contacts (first_name, last_name, init_date) values ("Al", "Pacino", "1940-04-24");
-insert into contacts (first_name, last_name, init_date) values ("Cate", "Blanchett	", "1969-05-13");
-insert into contacts (first_name, last_name, init_date) values ("Ewan", "McGregor", "1971-03-30");
-insert into contacts (first_name, last_name, init_date) values ("Rooney", "Mara", "1943-02-08");
-insert into contacts (first_name, last_name, init_date) values ("Joe", "Pesci", "1947-07-27");
+insert into contacts (first_name, last_name, init_date) values ('Clint', 'Eastwood', '1930-05-30');
+insert into contacts (first_name, last_name, init_date) values ('Arnold', 'Schwarzenegger', '1947-07-27');
+insert into contacts (first_name, last_name, init_date) values ('Jennifer', 'Connelly', '1970-12-11');
+insert into contacts (first_name, last_name, init_date) values ('Al', 'Pacino', '1940-04-24');
+insert into contacts (first_name, last_name, init_date) values ('Cate', 'Blanchett', '1969-05-13');
+insert into contacts (first_name, last_name, init_date) values ('Ewan', 'McGregor', '1971-03-30');
+insert into contacts (first_name, last_name, init_date) values ('Rooney', 'Mara', '1943-02-08');
+insert into contacts (first_name, last_name, init_date) values ('Joe', 'Pesci', '1947-07-27');
